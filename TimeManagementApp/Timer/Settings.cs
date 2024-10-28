ddcc030620666c86422dbf2081627186b3d5b4ea
cyanpororo <phananhhao22012004@gmail.com>
2024-10-28 22:47:51 +0700

    #3 - timer page (notification sound & toast)

--- TimeManagementApp/Timer/Settings.cs
+++ TimeManagementApp/Timer/Settings.cs
@@ -1,23 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TimeManagementApp.Timer
 {
-    public class Settings
+    public class Settings : INotifyPropertyChanged
     {
         public int FocusTimeMinutes { get; set; }
         public int ShortBreakMinutes { get; set; }
         public int LongBreakMinutes { get; set; }
+
+        public bool IsNotificationOn { get; set; }
 
         public Settings()
         {
             FocusTimeMinutes = 2;
             ShortBreakMinutes = 3;
             LongBreakMinutes = 5;
+
+            IsNotificationOn = true;
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 
     public enum TimerType
@@ -26,5 +33,4 @@ namespace TimeManagementApp.Timer
         ShortBreak,
         LongBreak,
     }
-
 }
